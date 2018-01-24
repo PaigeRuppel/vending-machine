bd63d3f6b0b9971b88358459f8dce9946fefb2b6
Paige Ruppel <paige.ruppel@gmail.com>
2018-01-24 12:50:59 -0500

    refactored display

--- src/main/java/com/paigeruppel/katas/vendingmachine/VendingDisplay.java
+++ src/main/java/com/paigeruppel/katas/vendingmachine/VendingDisplay.java
@@ -1,14 +1,19 @@
 package com.paigeruppel.katas.vendingmachine;
 
 public class VendingDisplay {
+
     private double insertedValue;
     private final String NO_VALUE_MESSAGE = "INSERT COIN";
 
     public String displayMessage() {
-        return (insertedValue == 0) ? NO_VALUE_MESSAGE : "" + insertedValue;
+        return (insertedValue == 0) ? NO_VALUE_MESSAGE : "" + displayInsertedValue();
     }
 
     public void addValue(double value) {
         insertedValue += value;
+    }
+
+    private String displayInsertedValue() {
+        return "Amount deposited: " + insertedValue;
     }
 }
