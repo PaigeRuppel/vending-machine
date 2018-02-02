3f6dc746c36eeaa2c617835543c44960ec4e03e0
Paige Ruppel <paige.ruppel@gmail.com>
2018-02-01 21:06:23 -0500

    working on mocking in coin slot test

--- src/test/java/com/paigeruppel/katas/vendingmachine/CoinSlotTest.java
+++ src/test/java/com/paigeruppel/katas/vendingmachine/CoinSlotTest.java
@@ -2,30 +2,32 @@ package com.paigeruppel.katas.vendingmachine;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
 
 import static com.paigeruppel.katas.vendingmachine.Coin.*;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.*;
 
 public class CoinSlotTest {
 
     private CoinSlot underTest;
 
-
     private CoinAcceptor validCoinAcceptor;
-
     private CoinAcceptor invalidCoinAcceptor;
 
     @Before
     public void setup() {
-        validCoinAcceptor = new CoinHolder();
-        invalidCoinAcceptor = new CoinReturn();
+        validCoinAcceptor = mock(CoinHolder.class);
+        invalidCoinAcceptor = mock(CoinReturn.class);
         underTest = new CoinSlot(validCoinAcceptor, invalidCoinAcceptor);
     }
 
     @Test
-    public void nickelIsValid() {
-        assertTrue(underTest.isValid(NICKEL));
+    public void whenNickelIsAcceptedValidCoinAcceptorShouldReceiveCoin() {
+        doNothing().when(validCoinAcceptor).accept(NICKEL);
+
     }
 
     @Test
