7e8f5b25e39a8b2e17cc7d2666150ba71cf448a7
Paige Ruppel <paige.ruppel@gmail.com>
2018-02-03 11:43:57 -0500

    added tests and logic for sensor to weigh and measure thickness and diameter of coin

--- src/main/java/com/paigeruppel/katas/vendingmachine/Sensor.java
+++ src/main/java/com/paigeruppel/katas/vendingmachine/Sensor.java
@@ -6,4 +6,12 @@ public class Sensor {
     public Double weigh(Coin coin) {
         return coin.getWeightInGrams();
     }
+
+    public Double measureDiameter(Coin coin) {
+        return coin.getDiameterInMm();
+    }
+
+    public Double measureThickness(Coin coin) {
+        return coin.getThicknessInMm();
+    }
 }
--- src/test/java/com/paigeruppel/katas/vendingmachine/SensorTest.java
+++ src/test/java/com/paigeruppel/katas/vendingmachine/SensorTest.java
@@ -3,9 +3,12 @@ package com.paigeruppel.katas.vendingmachine;
 import org.junit.Before;
 import org.junit.Test;
 
+import static com.paigeruppel.katas.vendingmachine.Coin.DIME;
 import static com.paigeruppel.katas.vendingmachine.Coin.NICKEL;
+import static com.paigeruppel.katas.vendingmachine.Coin.QUARTER;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 public class SensorTest {
 
@@ -15,5 +18,17 @@ public class SensorTest {
     public void sensorShouldCorrectlyWeighCoin() {
         underTest = new Sensor();
         assertThat(underTest.weigh(NICKEL), is(NICKEL.getWeightInGrams()));
+    }
+
+    @Test
+    public void sensorShouldCorrectlyMeasureDime() {
+        underTest = new Sensor();
+        assertThat(underTest.measureDiameter(DIME), is(DIME.getDiameterInMm()));
+    }
+
+    @Test
+    public void sensorShouldCorrectlyMeasureThicknessOfQuarter() {
+        underTest = new Sensor();
+        assertThat(underTest.measureThickness(QUARTER), is(QUARTER.getThicknessInMm()));
     }
 }
