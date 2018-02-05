6c1eb3216171f05cffb75dd9c93599e00dcbc5aa
Paige Ruppel <paige.ruppel@gmail.com>
2018-02-05 14:33:32 -0500

    refactored candyrack to implement productrack

--- src/main/java/com/paigeruppel/katas/vendingmachine/CandyRack.java
+++ src/main/java/com/paigeruppel/katas/vendingmachine/CandyRack.java
@@ -3,7 +3,7 @@ package com.paigeruppel.katas.vendingmachine;
 import java.util.ArrayList;
 import java.util.List;
 
-public class CandyRack {
+public class CandyRack implements ProductRack {
 
     private List<Product> inventory;
     private final Integer MAX_CAPACITY = 20;
@@ -16,6 +16,16 @@ public class CandyRack {
         for (int i = inventory.size(); i < MAX_CAPACITY; i++) {
             inventory.add(new Candy());
         }
+    }
+
+    @Override
+    public boolean isSoldOut() {
+        return inventory.isEmpty();
+    }
+
+    @Override
+    public void dispense() {
+        inventory.remove(0);
     }
 
     public Integer availableNumberProducts() {
--- src/test/java/com/paigeruppel/katas/vendingmachine/CandyRackTest.java
+++ src/test/java/com/paigeruppel/katas/vendingmachine/CandyRackTest.java
@@ -3,7 +3,9 @@ package com.paigeruppel.katas.vendingmachine;
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 public class CandyRackTest {
 
@@ -14,5 +16,35 @@ public class CandyRackTest {
         underTest = new CandyRack();
         underTest.stock();
         assertThat(underTest.availableNumberProducts(), is(20));
+    }
+
+    @Test
+    public void unstockedCandyRackShouldBeSoldOut() {
+        underTest = new CandyRack();
+        assertTrue(underTest.isSoldOut());
+    }
+
+    @Test
+    public void stockedCandyRackIsNotSoldOut() {
+        underTest = new CandyRack();
+        underTest.stock();
+        assertFalse(underTest.isSoldOut());
+    }
+
+    @Test
+    public void dispenseShouldRemoveOneCandyFromInventory() {
+        underTest = new CandyRack();
+        underTest.stock();
+        underTest.dispense();
+        assertThat(underTest.availableNumberProducts(), is(19));
+    }
+
+    @Test
+    public void twoDispensesShouldRemoveTwoCandiesFromInventory() {
+        underTest = new CandyRack();
+        underTest.stock();
+        underTest.dispense();
+        underTest.dispense();
+        assertThat(underTest.availableNumberProducts(), is(18));
     }
 }
